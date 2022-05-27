cad94947df8bf3c8f9f0690c383de5cbc327cee0
Mattsll99 <matthiassylla21@gmail.com>
2022-05-27 21:18:55 +0200

    loop

--- test/SYLIUMOracle-test/SYLIUMOracle-test.js
+++ test/SYLIUMOracle-test/SYLIUMOracle-test.js
@@ -24,15 +24,22 @@ describe("SYLIUM Oracle", function () {
   });
 
   it("Should return the ETH price and the value of the ETH reserve", async function () {
+    console.log("========== ETH PRICE IN WEI ==========");
     let ethPrice = await syliumOracle.get_eth_price();
-    console.log(ethPrice);
+    console.log("ETH price in WEI: " + ethPrice);
+    let dEthPrice = ethers.utils.formatEther(ethPrice);
+    console.log("ETH price: " + dEthPrice);
+    console.log("========== ETH RESERVE IN WEI ==========");
     let ethReserve = await syliumOracle.get_eth_reserve();
     console.log(ethReserve);
   });
 
   it("Should return the USDC price and the value of the USDC reserve", async function () {
+    console.log("========== USDC PRICE IN WEI ==========");
     let usdcPrice = await syliumOracle.get_usdc_price();
-    console.log(usdcPrice);
+    console.log("USDC price in WEI: " + usdcPrice);
+    let dUsdcPrice = ethers.utils.formatEther(usdcPrice);
+    console.log("USDC price: " + dUsdcPrice);
     let usdcReserve = await syliumOracle.get_usdc_reserve();
     console.log(usdcReserve);
   });
@@ -40,7 +47,9 @@ describe("SYLIUM Oracle", function () {
   it("Should return the SYLIX price and the value of the SYLIX reserve", async function () {
     let sylixPrice = await syliumOracle.get_sylix_price();
     console.log("========== SYLIX PRICE ==========");
-    console.log(sylixPrice);
+    console.log("SYLIX price in WEI: " + sylixPrice);
+    let dSylixPrice = ethers.utils.formatEther(sylixPrice);
+    console.log("SYLIX price: " + dSylixPrice);
     let sylixReserve = await syliumOracle.get_sylix_reserve();
     console.log("========== SYLIX RESERVE IN USD ==========");
     console.log(sylixReserve);
