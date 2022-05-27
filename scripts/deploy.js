6cdea9f8969766aa4d4a76445fd226e5ee53f245
Mattsll99 <matthiassylla21@gmail.com>
2022-05-27 21:16:47 +0200

    loop

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -17,9 +17,29 @@ async function main() {
   console.log("SYLIUMOracle deployed to: ", syliumOracle.address);
 
   const Aquafina = await hre.ethers.getContractFactory("Aquafina");
-  const aquafina = await Aquafina.deploy(1650456000);
+  const aquafina = await Aquafina.deploy(1650456000, 10);
   await aquafina.deployed();
   console.log("Aquafina deployed to: ", aquafina.address);
+
+  const syETH = await hre.ethers.getContractFactory("syETH");
+  const syEth = await syETH.deploy();
+  await syEth.deployed();
+  console.log("syEth deployed to: ", syEth.address);
+
+  const syUSDC = await hre.ethers.getContractFactory("syUSDC");
+  const syUsdc = await syUSDC.deploy();
+  await syUsdc.deployed();
+  console.log("syUsdc deployed to: ", syUsdc.address);
+
+  const SYLIX = await hre.ethers.getContractFactory("SYLIX");
+  const sylix = await SYLIX.deploy();
+  await sylix.deployed();
+  console.log("sylix deployed to: ", sylix.address);
+
+  const SYLIUM = await hre.ethers.getContractFactory("SYLI");
+  const sylium = await SYLIUM.deploy();
+  await sylium.deployed();
+  console.log("sylium deployed to: ", sylium.address);
 }
 
 main()
